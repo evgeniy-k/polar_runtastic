4e2fd9f26bc4f28b3e0798107b355623e432a0ae
Evgeniy Kozin <evgeniy.kozin@gmail.com>
2014-01-01 14:34:30 +0400

    Итоговая версия с экспортом в TCX

--- ran.py
+++ ran.py
@@ -1,16 +1,27 @@
 # -*- coding: utf-8 -*-
-import  os
-import  re
-from datetime import datetime
+import os
+import re
+import datetime
 
 exfiles = []
-weightList = [{'date': datetime.now(), 'val': 0.0}]
+weightList = [{'date': datetime.datetime.now(), 'val': 0.0}]
 exerciseList = []
+
+tcxTemplate = '<?xml version="1.0" encoding="UTF-8"?>\n\
+<TrainingCenterDatabase version="1.0" creator="runtastic - makes sports funtastic, http://www.runtastic.com" \
+xsi:schemaLocation="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2 http://www.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd" \
+xmlns:ns5="http://www.garmin.com/xmlschemas/ActivityGoals/v1" xmlns:ns3="http://www.garmin.com/xmlschemas/ActivityExtension/v2" \
+xmlns:ns2="http://www.garmin.com/xmlschemas/UserProfile/v2" xmlns="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2" \
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ns4="http://www.garmin.com/xmlschemas/ProfileExtension/v1">\n\
+<Activities>\n\t<Activity Sport="{Activity}">\n\t\t<Id>{StartTime}</Id>\n\t\t<Lap StartTime="{StartTime}">\n\t\t\t<TotalTimeSeconds>{TotalTimeSeconds}</TotalTimeSeconds> \
+\n\t\t\t<DistanceMeters>{DistanceMeters}</DistanceMeters>\n\t\t\t<Calories>{Calories}</Calories>\n\t\t\t<AverageHeartRateBpm><Value>{AverageHeartRateBpm}</Value>\
+</AverageHeartRateBpm>\n\t\t\t<MaximumHeartRateBpm><Value>{MaximumHeartRateBpm}</Value></MaximumHeartRateBpm>\n\t\t</Lap>\n\t</Activity>\n</Activities>\n</TrainingCenterDatabase>'
 
 hrmpathRE = re.compile ('(.+\/).+\.pdd', re.DOTALL)
 exerRE = re.compile ('\[ExerciseInfo(\d)\](.+?)(\d{8}\.hrm)', re.DOTALL)
 weightRE = re.compile ('.+Weight=(\d+).+', re.DOTALL)
 datetimeRE = re.compile ('.+Date=(\d{4})(\d{2})(\d{2}).+StartTime=(\d{1,2}):(\d{2}):(\d{2}).+', re.DOTALL)
+lengthRE = re.compile ('.+Length=(.+?)\n', re.DOTALL)
 
 # Получаем список файлов с тренировками 
 for dirname, dirnames, filenames in os.walk('data/'):
@@ -33,6 +44,7 @@ for fpath in exfiles:
   for match in iterator:
     # Обнуляем временные значения
     weight = 0
+    trlength = ''
     date_object = None
     exerciseInfo = None
     hrmPath = None
@@ -41,13 +53,26 @@ for fpath in exfiles:
     exerciseInfo = match.group(2)
     hrmPath = '%s%s' % (rootpath, match.group(3))
     
+    sport = (int)(exerciseInfo.split('\n')[3].split('\t')[0])
+    if sport == 1:
+      sport = 'running'
+    elif sport == 2:
+      sport = 'cycling'
+    else:
+      sport = None
+    
+    calories = (int)(exerciseInfo.split('\n')[3].split('\t')[5])
+    distance = (int)(exerciseInfo.split('\n')[4].split('\t')[0])
+    averageHeartRateBpm = (int)(exerciseInfo.split('\n')[10].split('\t')[0])
+    maximumHeartRateBpm = (int)(exerciseInfo.split('\n')[10].split('\t')[1])
+    
     # Читаем содержимое файла тренировки
     hrm_f = open(hrmPath, 'r')
     hrm_content = hrm_f.read()
     
     # Вытаскиваем дату занятия
     res = datetimeRE.match (hrm_content)
-    date_object = datetime.strptime('%s/%s/%s %s:%s:%s' 
+    date_object = datetime.datetime.strptime('%s/%s/%s %s:%s:%s' 
     % (res.group(1),res.group(2),res.group(3), res.group(4),res.group(5),res.group(6)),
     '%Y/%m/%d %H:%M:%S')
     
@@ -56,14 +81,27 @@ for fpath in exfiles:
     weight = res.group(1)
     if weightList[-1]['val'] != float(weight):
       weightList.append ({'date': date_object, 'val': float(weight)})
-      
+
+    # Вытаскиваем продолжительность занятия
+    res = lengthRE.match (hrm_content)
+    trlength = res.group(1)[:-3]
+    tmp = trlength.split(':')
+    trlength_sec = ((int)(tmp[0]) * 60 * 60) + ((int)(tmp[1]) * 60) + (int)(tmp[2])
+    
+            
     # Заполняем даные о тренировке
-    exerciseList.append ({'date' : date_object,
-    })
+    if sport:
+      exerciseList.append ({'date' : date_object,
+                          'length' : trlength_sec,
+                          'sport' : sport,
+                          'calories' : calories,
+                          'distance' : distance,
+                          'averageHeartRateBpm' : averageHeartRateBpm,
+                          'maximumHeartRateBpm' : maximumHeartRateBpm,
+      })
     
     #Закрываем hrm-файл
     hrm_f.close()
-    
   #Закрываем pdd-файл
   pdd_f.close()
   
@@ -71,7 +109,15 @@ for fpath in exfiles:
 # Выводим тренировки
 print '### Показатели тренировок:'
 for i in exerciseList:
-  print '%.2d.%.2d.%s, %.2d:%.2d' % (i['date'].day, i['date'].month, i['date'].year, i['date'].hour, i['date'].minute)
+  print '%s - %s %ds %dm %dc %dBpm/%dBpm' % ((i['date'] - datetime.timedelta(hours = 4)).strftime("%Y-%m-%dT%H:%M:%S.000Z"),
+  i['sport'], i['length'], i['distance'], i['calories'], i['averageHeartRateBpm'], i['maximumHeartRateBpm'])
+  out_f = open('out/%s-%s.tcx' % (i['sport'], i['date'].strftime("%Y%m%d%H%M")), 'w')
+  out_content = tcxTemplate
+  out_content = out_content.replace('{Activity}', i['sport']).replace('{StartTime}', (i['date'] - datetime.timedelta(hours = 4)).strftime('%Y-%m-%dT%H:%M:%S.000Z'))
+  out_content = out_content.replace('{TotalTimeSeconds}', str(i['length'])).replace('{DistanceMeters}', str(i['distance'])).replace('{Calories}', str(i['calories']))
+  out_content = out_content.replace('{AverageHeartRateBpm}', str(i['averageHeartRateBpm'])).replace('{MaximumHeartRateBpm}', str(i['maximumHeartRateBpm']))
+  out_f.write( out_content)
+  out_f.close()
   
 # Выводим вес
 print '### Изменение веса:'
